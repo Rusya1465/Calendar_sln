d12a516edd4267ffa5a18c3e52a3462aef131b46
Rusya <85542333+Rusya1465@users.noreply.github.com>
2024-05-20 11:16:37 +0600

    Добавьте файлы проекта.

---
+++ Calendar/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Calendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dayContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnnext = new System.Windows.Forms.Button();
+            this.btnprev = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dayContainer
+            // 
+            this.dayContainer.Location = new System.Drawing.Point(35, 82);
+            this.dayContainer.Name = "dayContainer";
+            this.dayContainer.Size = new System.Drawing.Size(1305, 622);
+            this.dayContainer.TabIndex = 0;
+            // 
+            // btnnext
+            // 
+            this.btnnext.Location = new System.Drawing.Point(1237, 746);
+            this.btnnext.Name = "btnnext";
+            this.btnnext.Size = new System.Drawing.Size(103, 46);
+            this.btnnext.TabIndex = 1;
+            this.btnnext.Text = "Вперед";
+            this.btnnext.UseVisualStyleBackColor = true;
+            // 
+            // btnprev
+            // 
+            this.btnprev.Location = new System.Drawing.Point(1110, 746);
+            this.btnprev.Name = "btnprev";
+            this.btnprev.Size = new System.Drawing.Size(106, 46);
+            this.btnprev.TabIndex = 2;
+            this.btnprev.Text = "Назад";
+            this.btnprev.UseVisualStyleBackColor = true;
+            this.btnprev.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Понедельник";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вторник";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Среда";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(651, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Четверг";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(836, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Пятница";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1029, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Субота";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1192, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Воскресенье";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 804);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnprev);
+            this.Controls.Add(this.btnnext);
+            this.Controls.Add(this.dayContainer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel dayContainer;
+        private System.Windows.Forms.Button btnnext;
+        private System.Windows.Forms.Button btnprev;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Calendar/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            displaDays();
+        }
+        private void displaDays()
+        {
+            DateTime now = DateTime.Now;
+
+            DateTime startofthemonth = new DateTime(now.Year, now.Month, 1);
+            int days = DateTime.DaysInMonth(now.Year, now.Month);
+            int dayoftheweek = Convert.ToInt32(startofthemonth.DayOfWeek.ToString("d")) + 1;
+            
+            for(int i = 1; i < dayoftheweek; i++)
+            {
+                UserControl1 ucblank = new UserControl1();
+                dayContainer.Controls.Add(ucblank);
+            }
+        }
+    }
+}
